138b6271875587e4a7673eb2f536e3bc2ae5c17e
farhiya wali duad <120467355+farhiya-wali-daud@users.noreply.github.com>
2023-01-29 09:27:24 +0300

    Add files via upload
    Hey guys, this is our group project,  I hope you like it.

---
+++ farhiya,Amal,Mumtaaz.py
@@ -0,0 +1,46 @@
+'''#1
+
+s=str(input("put your name here"))
+n=int(input("put the number here"))
+for each in range (n):
+    print(s)
+#2
+
+for k in range(2,20):
+    if k==9:
+        continue
+    else:
+        print(k)
+
+#3
+'''    
+
+import turtle
+z=turtle.Turtle("turtle")
+
+z.forward(120)
+z.left(36)
+z.forward(120)
+z.left(36)
+z.forward(120)
+z.left(36)
+z.forward(120)
+z.left(36)
+z.forward(120)
+z.left(36)
+z.forward(120)
+z.left(36)
+z.forward(120)
+z.left(36)
+z.forward(120)
+z.left(36)
+z.forward(120)
+z.left(36)
+z.forward(120)
+
+# without comment only the first part is running becouse the are totlly differnet project
+# if you put commnet second part is working
+
+
+
+
